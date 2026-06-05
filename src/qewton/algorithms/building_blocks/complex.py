5336ec9f1a2e6e00a5b12b80ac1dc68fdaf93476
Uladzislau Kapustsin <uladzislau.kapustsin@ovgu.de>
2026-06-05 13:46:21 +0200

    feat: implement ComplexValued node

--- src/qewton/algorithms/building_blocks/complex.py
+++ src/qewton/algorithms/building_blocks/complex.py
@@ -4,8 +4,12 @@ import tensorflow as tf
 import torch
 
 from qewton.algorithms.backend_node import BackendNode, TensorType
+from qewton.algorithms.building_blocks.utility import Cast
 from qewton.config.axes import EllipsisAxes
+from qewton.config.backend import Backend
 from qewton.config.data_configurations import DataConfiguration
+from qewton.graphs.control_nodes.graph_node import CopiedNode
+from qewton.graphs.nodes import Node
 
 
 class ReIm(BackendNode[TensorType]):
@@ -26,3 +30,42 @@ class ReIm(BackendNode[TensorType]):
 
     def tensorflow_implementation(self, x: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor]:
         return self.backend.library.math.real(x), self.backend.library.math.imag(x)
+
+
+class ComplexValued(TrackedNode):
+    fr: CopiedNode
+    fi: CopiedNode
+    fr2: CopiedNode
+    fi2: CopiedNode
+    reim: ReIm
+    cast: Cast
+    cast2: Cast
+
+    def __init__(
+        self,
+        module: Node,
+        backend: type[Backend[TensorType]] = DEFAULT_DL_BACKEND,
+        name: str = "complex_valued",
+    ):
+        self.fr = CopiedNode(module)
+        self.fi = CopiedNode(module)
+        self.fr2 = CopiedNode(module)
+        self.fi2 = CopiedNode(module)
+        self.reim = ReIm(backend=backend)
+        self.cast = Cast(backend=backend)
+        self.cast2 = CopiedNode(self.cast)
+        super().__init__(name, backend)
+
+    def forward(
+        self, x: Annotated[TensorType, DataConfiguration.empty()]
+    ) -> Annotated[TensorType, DataConfiguration.empty()]:
+        re, im = self.reim(x)
+
+        return self.cast2(
+            self.cast(
+                self.fr(re) - self.fi(im),
+                "complex64",
+            )
+            + 1j * (self.fr2(im) + self.fi2(re)),
+            "complex64",
+        )
